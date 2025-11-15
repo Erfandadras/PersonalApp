518d7da92526488e58799d1a228a4d5ca0382d92
Erfan dadras <erfan.d@pirelly.com>
2025-11-15 17:48:11 +0330

    setup injection, setup user default manager

---
+++ Modules/BaseModule/Sources/BaseModule/Injection/CoreDependence.swift
@@ -0,0 +1,29 @@
+//
+//  CoreDependence.swift
+//  BaseModule
+//
+//  Created by Erfan mac mini on 11/15/25.
+//
+
+import UIKit
+
+public class CoreDependence: DependenceProviders {
+    // MARK: - properties
+    // make sure this class execute on the start
+    var application: UIApplication?
+    private let container = DependencyContainer.shared
+    
+    // MARK: - init
+    public init(_ app: UIApplication) {}
+
+    // MARK: - logic
+    public func execute() {
+        let userManager = UserManager()
+        container.register(userManager, for: UserManager.self)
+    }
+    
+    public func reset() {
+        @Injected var userManager: UserManager
+        userManager.deleteUser()
+    }
+}
